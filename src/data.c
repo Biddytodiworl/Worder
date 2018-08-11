35c0a37a4021c60cb80361c4bf20ffcf2fce565b
Brendon Henry <biddytodiworl@gmail.com>
2018-08-10 21:48:35 -0500

    Remove extra LICENCE header

--- src/data.c
+++ src/data.c
@@ -20,29 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *//* Worder - Learning application geared towards French improvement
-  * MIT License
-  *
-  * Copyright (c) 2018 Brendon Henry
-  *
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to deal
-  * in the Software without restriction, including without limitation the rights
-  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  * copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in all
-  * copies or substantial portions of the Software.
-  *
-  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  * SOFTWARE.
-  */
+ */
 
 #include "data.h"
 #include <stdbool.h>
@@ -90,14 +68,13 @@ uint32_t Data_RetrieveWord(Word *dst, const uint32_t index) {
 uint32_t Data_CalculateWordEntries(uint32_t *num) {
 	/* Open the dict.txt file and check for failure */
 	FILE *dict = fopen("dict.txt", "r");
-	if (NULL == dict) return DATA_ERR;
+	if (NULL == dict) return DATA_FILE_ERR;
 
 	/* Start counting the lines */
 	uint32_t lines = 0;
-	char buffer[100];
+	char buffer[50];
 
 	while (NULL != (fgets(buffer, 50, dict))) {
-		// if (3 == fscanf(dict, "%" SCNu32 " %s %s", &index, fr, en))
 			++lines;
 	}
 
